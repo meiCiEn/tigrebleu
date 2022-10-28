0fac390363767f605afb9b6151cc98cbe8099da3
meiCiEn <eleanor.mears@gmail.com>
2022-10-28 15:03:59 +0200

    learn and play

---
+++ View/lessons/lesson-num-1.php
@@ -0,0 +1,107 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/Assets/css/reset.css">
+        <link rel="stylesheet" href="/Assets/css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <title>Document</title>
+    </head>
+
+<body>
+    <section class="section section--page">
+        <div class="page__inner d-flex">
+            <div class="text-box">
+                <div d-flex></div>
+                <h2 class="h2--lesson">Apprendre : Les chiffres</h2><div>
+                <h3 class="h3--lesson">Leçon 1 :Un,deux,trois </h3>
+        <div class="page__inner d-flex">
+            <div class="text-box2">
+                <p>Les mots en chinois sont représentés par des caractères… Ce ne sont pas des lettres !</p>
+                <p>Pour les chiffres, chaque chiffre de zéro à dix est représenté par un caractère.</p>
+                <p>Le chiffre un est représenté par un simple trait horizontal.</p>
+                   <p>Cliquez sur le caractère pour entendre sa prononciation !</p> 	 
+                    <p> Pour le chiffre deux, il faut deux traits !</p>
+                    <p>Celui du dessus est plus court.	 </p>
+                    <p>Et pour le chiffre trois… Trois traits !</p>
+                    <p>Le trait du milieu est le plus court, et celui du dessous le plus long.	 </p>
+                    </p>
+            </div>
+            <div class="lesson">
+            <h3 class="h3--lesson">Leçon 2 De 0 à 10</h3>
+                <div class="text-box2">
+                    <p>Pour les autres chiffres (zéro et de quatre à dix),</p>
+                    <p>c’est un peu plus difficile. On notera que le caractère pour cinq comporte cinq traits,</p> 
+                     <p>et que le caractère pour sept ressemble au chiffre 7 retourné !</p>
+                </div>
+       </div>
+        </div>
+        
+        <div class="tigre">
+            <img src="/Assets/images/deco/tigre-bleu-lg.svg" alt="tigre bleu">
+        </div>
+
+<div class="table">
+<table>
+    <thead>
+      <caption></caption>
+    </thead>
+    <tbody>
+      <tr>
+        <th>4</th> 
+        <td></td>
+        <td></td> 
+      </tr>
+      <tr>
+        <th>5</th>
+        <td></td>  
+        <td></td> 
+      </tr>
+      <tr>
+        <th>6</th> 
+        <td></td> 
+        <td></td> 
+      </tr>
+      <tr>
+        <th>7</th>  
+        <td></td>
+        <td></td> 
+      </tr>
+      <tr>
+        <th>8</th>
+        <td></td>  
+        <td></td> 
+      </tr>
+      <tr>
+        <th>9</th> 
+        <td></td> 
+        <td></td> 
+      </tr>
+      <tr>
+        <th>10</th> 
+        <td></td> 
+        <td></td> 
+      </tr>
+      <tr>
+       
+         
+      </tr>
+    </tbody>
+    </table>
+</div>
+            </div>
+        </div>
+        <div class="tigre">
+            <img src="/Assets/images/deco/tigre-bleu-lg.svg" alt="tigre bleu">
+        </div>
+    </section>
+
+    </body>
+    </html>
